fe5f8efe5969cbfb0e551152d243ffe6795c2ccc
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2015-03-12 07:34:48 +0100

    modified commentaries 	modified:   main.js

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ var client = new irc.Client('chat.freenode.org', 'D8ta', {
     channels: ['#schnitzelwirt']
 });
 
+// shows message and name
 client.addListener('message', function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
     $('#chatwindow').append('<p>' + from + ' => ' + to + ': ' + message + '</p>');
 });
 
+// throw error message if nessecary
 client.addListener('error', function(message) {
     console.log('error: ', message);
 });
 
-// Namelist
+// Namelist via Object.keys from the Object
 client.addListener('names', function (channel, names) {
     var chatter = Object.keys(names);
     console.log(chatter);
@@ -20,15 +22,19 @@ client.addListener('names', function (channel, names) {
 
 });
 
+// function for texting to the IRC chat
 function say() {
     var speak = document.getElementById("text").value;
     $('#chatwindow').append('<p> me =>' + speak + '</p>');
     client.say('#schnitzelwirt', speak);
 }
 
+// disconnecet from IRC
 function disco() {
     client.part('#schnitzelwirt');
+}
 
-}function connect() {
+// connect to IRC
+function connect() {
     client.join('#schnitzelwirt nodewebkit');
 }
